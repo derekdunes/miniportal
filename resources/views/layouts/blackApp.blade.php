ba2b515d95d1a542db03b7f504b58ee7201ab128
DEREKDUNES <mbahderek@gmail.com>
2020-07-18 00:54:11 +0100

    modifies welcome page to use blackApp

--- resources/views/layouts/blackApp.blade.php
+++ resources/views/layouts/blackApp.blade.php
@@ -37,7 +37,7 @@
     <div class="d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="masthead mb-auto">
     <div class="inner">
-      <h3 class="masthead-brand"><a href="{{ url('/')}}">MiniPort</a></h3>
+      <h3 class="masthead-brand"><a href="{{ url('/')}}">MiniPortal</a></h3>
       <nav class="nav nav-masthead justify-content-center">
         @guest
              @if (Route::has('login'))
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,60 +1,8 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v4.0.1">
-    <title>Mini Portal</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/cover/">
+@extends('layouts.blackApp')
 
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+@section('content')
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('css/cover.css') }}" rel="stylesheet">
-
-  </head>
-  <body class="text-center">
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <header class="masthead mb-auto">
-    <div class="inner">
-      <h3 class="masthead-brand">MiniPort</h3>
-      <nav class="nav nav-masthead justify-content-center">
-         @if (Route::has('login'))
-            @auth
-                <a href="{{ url('/home') }}" class="nav-link active">Home</a>
-            @else
-                <a href="{{ route('login') }}" class="nav-link">Login</a>
-
-                @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="nav-link">Register</a>
-                @endif
-            @endauth
-        @endif
-      </nav>
-    </div>
-  </header>
-
-  <main role="main" class="inner cover">
     <h1 class="cover-heading">MiniPortal</h1>
     <p class="lead">Login or Register to start adding students and creating Courses</p>
     <p class="lead">
@@ -62,13 +10,5 @@
         
         <a href="{{ route('register') }}" class="btn btn-lg btn-dark">Register</a>
     </p>
-  </main>
 
-  <footer class="mastfoot mt-auto">
-    <div class="inner">
-      <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-    </div>
-  </footer>
-</div>
-</body>
-</html>
+@endsection
